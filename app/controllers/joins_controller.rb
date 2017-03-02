95ede295181ee52c6ce9d4d25368e5f356ae6a3f
Scott Helms <shelms@imageoptions.net>
2017-03-01 16:16:32 -0800

    unless refactor on joins controller create

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -3,23 +3,14 @@ class JoinsController < ApplicationController
 
   def create
     join = Join.new( join_params )
-    if join.save
-      # puts "saved"
-      redirect_to events_path
-    else
-      flash[:errors] = join.errors.full_messages
-      # puts "not saved, brah"
-      redirect_to events_path
-    end
+    flash[:errors] = join.errors.full_messages unless join.save
+    redirect_to events_path
   end
 
   def destroy
     join = Join.where( join_params )
     join_id = Join.find_by(user: current_user, event: (join_params[:event_id]))
-    # puts "Event ID: #{join_params[:event_id]}"
-    # puts "User ID: #{join_params[:user_id]}"
     Join.find(join_id.id).destroy
-    # puts "Join ID: #{Join.user_id}"
     redirect_to events_path
   end
 
